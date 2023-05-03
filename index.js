c694dbc81cf8bb96cb2b941aac892e45d56f9d1e
Remon Tripura <remontripura045@gmail.com>
2023-05-03 12:17:19 +0600

    cors install

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 5000;
 
 const chefdata = require('./data/chefdata.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('chef recipe server is running')
